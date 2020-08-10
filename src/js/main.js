a545e88130f16c212fd3bc377fd4acc8fa9f88b3
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-08-10 15:05:41 +0400

    feat: add slider.js

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import Slider from "./modules/slider";
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   const slider = new Slider(".page", ".next");
+//   slider.render();
+// });
+
+window.addEventListener('DOMContentLoaded', () => {
+  const slider = new Slider('.page', '.next');
+  slider.render()
+})
